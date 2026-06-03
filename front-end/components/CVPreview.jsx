8d4f8a887465e3d9f88e8d763f1dd6db971a32aa
ThijsVanLoo1 <thijs30042002@gmail.com>
2026-06-03 15:40:55 +0200

    Layout for the CV preview page. Back-end now functional

--- front-end/components/CVPreview.jsx
+++ front-end/components/CVPreview.jsx
@@ -1,31 +1,38 @@
 export function CVPreview({ data }) {
     return (
         <div>
-            <h1 className="font-bold">{data.name}</h1>
-
-            <section className="mt-8">
-                <h2 className="font-bold">Profiel</h2>
-                <p>{data.summary}</p>
+            <section className={"flex items-center gap-4 bg-primary text-outline p-4 rounded-t-lg"}>
+                <img src={data.image} alt={""} className={"h-20 w-20"} />
+                <div className={"flex flex-col"}>
+                    <h2>{data.name}</h2>
+                    <p>[Email]</p>
+                    <p>[Nummer]</p>
+                </div>
             </section>
 
-            <section className="mt-8">
-                <h2 className="font-bold">Skills</h2>
-                <div className="flex flex-wrap gap-2">
-                    {data.skills.map((skill) => (
-                        <span key={skill} className="rounded-lg">
-                          {skill}
-                        </span>
-                    ))}
-                </div>
-                <div className="flex flex-wrap gap-2">
-                    {data.strengths.map((s) => (
-                        <span key={s}>{s}</span>
-                    ))}
-                </div>
-                <div className="flex flex-wrap gap-2">
-                    {data.interests.map((i) => (
-                        <li key={i}>{i}</li>
-                    ))}
+            <section>
+                <p className={"text-sm py-4"}>{data.summary}</p>
+                <h2>Skills</h2>
+                <div>
+                    <div className={"flex gap-4 py-2"}>
+                        {data.skills.map((skill) => (
+                            <span key={skill} className={"bg-primary py-2 px-4 rounded-lg text-outline"}>
+                              {skill}
+                            </span>
+                        ))}
+                    </div>
+                    <h2>Strengths</h2>
+                    <div className={"flex flex-col py-2"}>
+                        {data.strengths.map((s) => (
+                            <span key={s}>{s}</span>
+                        ))}
+                    </div>
+                    <h2>Interests</h2>
+                    <div className={"flex flex-col py-2"}>
+                        {data.interests.map((i) => (
+                            <span key={i}>{i}</span>
+                        ))}
+                    </div>
                 </div>
             </section>
         </div>
