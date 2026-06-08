714687a0ef6aaab29e9cbcb667ad9e5bd47cf1dd
Semmetje <semvandenende@gmail.com>
2026-06-08 15:05:07 +0200

    Add chat feature styling to UserLayout with animation

--- front-end/layouts/UserLayout.jsx
+++ front-end/layouts/UserLayout.jsx
@@ -1,11 +1,15 @@
 import {Link, Outlet} from "react-router";
 import VintroLogo from "../src/assets/vintro_logo-03.png";
+import Victoria from "../src/assets/Victoria - Job coach.png";
 import {IoMdHome} from "react-icons/io";
 import {IoNewspaperSharp} from "react-icons/io5";
 import {FaMicrophoneAlt, FaUser} from "react-icons/fa";
 import {MdEditDocument} from "react-icons/md";
+import {useState} from "react";
 
 export default function UserLayout() {
+    const [chatOpen, setChatOpen] = useState(false);
+
     return (
         <>
             <header>
@@ -16,7 +20,19 @@ export default function UserLayout() {
             <main>
                 <Outlet/>
             </main>
+            {chatOpen &&
+                <section
+                    className={"fixed bottom-2.5 right-2.5 bg-body outline-body outline-5 border-primary border-3 rounded-lg h-[calc(100%-80px)] w-90 p-5 z-9999 animate-[visible_0.25s_ease-in-out]"}>
+                    hoi
+                </section>
+            }
             <footer className={"fixed left-0 right-0 bottom-0 z-999"}>
+                <div className={"flex justify-end p-2.5"}>
+                    <button className={"bg-primary shadow rounded-full w-1/5 hover:bg-primary-hover sm:w-1/20"}
+                            onClick={() => setChatOpen((x) => !x)}>
+                        <img src={Victoria} alt="Victoria - Chat Bot" className={"aspect-square w-full translate-y-1"}/>
+                    </button>
+                </div>
                 <nav className="grid grid-cols-5 bg-outline p-2">
                     {/* Divs will be Links when routes are available */}
                     <Link to={"/app"} className={"flex justify-center"}><IoMdHome size={40}/></Link>
