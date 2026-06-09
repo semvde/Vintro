7e909e48460bfd80446dccdbed57bdede2e92860
Semmetje <semvandenende@gmail.com>
2026-06-09 10:44:43 +0200

    Implement chat feature with message handling (no back-end yet) and animations

--- front-end/layouts/UserLayout.jsx
+++ front-end/layouts/UserLayout.jsx
@@ -6,9 +6,31 @@ import {IoNewspaperSharp} from "react-icons/io5";
 import {FaMicrophoneAlt, FaUser} from "react-icons/fa";
 import {MdEditDocument} from "react-icons/md";
 import {useState} from "react";
+import {FaXmark} from "react-icons/fa6";
+import FormField from "../components/FormField.jsx";
+import {MdOutlineQuestionAnswer} from "react-icons/md";
+import ChatMessage from "../components/ChatMessage.jsx";
 
 export default function UserLayout() {
     const [chatOpen, setChatOpen] = useState(false);
+    const [chatAnimation, setChatAnimation] = useState('visible');
+    const [chatMessages, setChatMessages] = useState([{
+        role: 'assistant',
+        content: 'Waar kan ik je mee helpen? Stel je vraag in een korte zin!'
+    }]);
+
+    function openChat() {
+        setChatAnimation('visible');
+        setChatOpen(true);
+    }
+
+    function closeChat() {
+        setChatAnimation('hide');
+
+        setTimeout(() => {
+            setChatOpen(false);
+        }, 500)
+    }
 
     return (
         <>
@@ -21,15 +43,26 @@ export default function UserLayout() {
                 <Outlet/>
             </main>
             {chatOpen &&
-                <section
-                    className={"fixed bottom-2.5 right-2.5 bg-body outline-body outline-5 border-primary border-3 rounded-lg h-[calc(100%-80px)] w-90 p-5 z-9999 animate-[visible_0.25s_ease-in-out]"}>
-                    hoi
+                <section id={"chat"}
+                         className={`fixed bottom-2.5 right-2.5 bg-[#E1F0DF] outline-[#E1F0DF] outline-5 border-primary border-3 rounded-lg h-[calc(100%-80px)] w-80 p-5 z-9999 animate-[${chatAnimation}_0.5s_ease-in-out]`}>
+                    <button onClick={() => closeChat()} className={"absolute right-5 top-5"}><FaXmark size={25}/>
+                    </button>
+                    <h2>Chat met Victoria</h2>
+                    <div className={"overflow-y-auto"}>
+                        {
+                            chatMessages.map((message) => <ChatMessage role={message.role} content={message.content}/>)
+                        }
+                    </div>
+                    <div className={"absolute bottom-0 left-0 right-0"}>
+                        <FormField icon={<MdOutlineQuestionAnswer/>}
+                                   placeholder={"Stel je vraag..."}/>
+                    </div>
                 </section>
             }
             <footer className={"fixed left-0 right-0 bottom-0 z-999"}>
                 <div className={"flex justify-end p-2.5"}>
                     <button className={"bg-primary shadow rounded-full w-1/5 hover:bg-primary-hover sm:w-1/20"}
-                            onClick={() => setChatOpen((x) => !x)}>
+                            onClick={() => openChat()}>
                         <img src={Victoria} alt="Victoria - Chat Bot" className={"aspect-square w-full translate-y-1"}/>
                     </button>
                 </div>
