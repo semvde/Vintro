4469fe06850574fd74488d17cc2e347331bc701d
Lisa <lisamao2007@gmail.com>
2026-06-10 13:31:19 +0200

    added dynamic data to account page but not submittable yet

--- front-end/pages/account/EditAccount.jsx
+++ front-end/pages/account/EditAccount.jsx
@@ -1,50 +1,98 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router";
-import { GoArrowLeft } from "react-icons/go";
+import {useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router";
+import {GoArrowLeft} from "react-icons/go";
+import {fetchAPI} from "../../services/Fetch.js";
 
 export default function EditAccount() {
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
 
     const [formData, setFormData] = useState({
-        naam: "Vincent",
+        naam: "",
         email: "",
-        wachtwoord: ""
+
     });
 
     const [errors, setErrors] = useState({});
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prev) => ({ ...prev, [name]: value }));
+        const {name, value} = e.target;
+        setFormData((prev) => ({...prev, [name]: value}));
 
         if (errors[name]) {
-            setErrors((prev) => ({ ...prev, [name]: "" }));
+            setErrors((prev) => ({...prev, [name]: ""}));
         }
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const newErrors = {};
 
-        if (!formData.naam.trim()) {
+        if (!formData.naam) {
             newErrors.naam = "Naam is verplicht.";
         }
-        if (!formData.email.trim()) {
+        if (!formData.email) {
             newErrors.email = "E-mailadres is verplicht.";
-        }
-        if (!formData.wachtwoord) {
-            newErrors.wachtwoord = "Wachtwoord is verplicht.";
-        } else if (formData.wachtwoord.length < 6) {
-            newErrors.wachtwoord = "Wachtwoord moet minimaal 6 tekens bevatten.";
         }
 
         if (Object.keys(newErrors).length > 0) {
             setErrors(newErrors);
         } else {
-            console.log("Form submitted successfully:", formData);
-            navigate("/account");
+            try {
+                setLoading(true)
+
+                const payload = {
+                    name: formData.name,
+                    email: formData.email,
+                }
+
+                console.log("Dit sturen we naar de backend:", payload);
+
+                const resData = await fetchAPI("/profile", "PUT", payload)
+                console.log("De ontvangen data is:", resData);
+
+                if (resData && resData.items && resData.items[0]?.error) {
+                    alert("Upload mislukt: " + resData.items[0].error)
+                } else if (resData) {
+                    console.log("Account geupdate!", resData)
+                    navigate("/app/account")
+                } else {
+                    alert("Upload mislukt: Er ging iets mis aan de serverkant.")
+                }
+            } catch
+                (error) {
+                console.error("Error met updaten:", error)
+            } finally {
+                setLoading(false)
+            }
+
         }
     };
+
+    useEffect(() => {
+        async function getAccountData() {
+            try {
+                const response = await fetchAPI("/profile");
+
+                if (response && response.data) {
+                    const profile = response.data.profile || {};
+                    const cv = response.data.cv || {};
+
+                    setFormData({
+                        name: profile.name || "",
+                        email: cv.email || "",
+                        phone_number: cv.phone_number || ""
+                    });
+                }
+            } catch (error) {
+                console.error("Account data laden mislukt:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getAccountData();
+    }, []);
 
     return (
         <>
@@ -55,9 +103,10 @@ export default function EditAccount() {
                     </Link>
                 </div>
                 <div className="bg-primary/20 pb-10 md:rounded-lg md:flex md:flex-row md:pb-0 md:items-stretch">
-                    <div className="bg-primary/20 flex flex-row md:flex-col items-center space-x-3 md:space-x-0 md:space-y-4 p-6 md:w-1/3 md:justify-center md:rounded-l-lg">
+                    <div
+                        className="bg-primary/20 flex flex-row md:flex-col items-center space-x-3 md:space-x-0 md:space-y-4 p-6 md:w-1/3 md:justify-center md:rounded-l-lg">
                         <img alt="profiel foto" src="/front-end/public"/>
-                        <h2>{formData.naam || "Gebruiker"}</h2>
+                        <h2>{formData.name || "Gebruiker"}</h2>
                     </div>
 
                     <div className="flex flex-col gap-4 md:w-2/3 md:p-6">
@@ -66,13 +115,14 @@ export default function EditAccount() {
                             <div className="flex justify-between flex-row items-center">
                                 <label className="h2 text-primary">Naam</label>
                                 <input
-                                    name="naam"
-                                    value={formData.naam}
+                                    name="name"
+                                    value={formData.name}
                                     onChange={handleChange}
                                     className={`pl-2 w-3/5 bg-white md:max-w-md h-10 rounded ${errors.naam ? 'border-2 border-red-500' : ''}`}
                                 />
                             </div>
-                            {errors.naam && <span className="text-red-600 text-sm font-medium text-right pr-2">{errors.naam}</span>}
+                            {errors.naam &&
+                                <span className="text-red-600 text-sm font-medium text-right pr-2">{errors.naam}</span>}
                         </div>
 
                         <div className="bg-secondary/20 p-6 flex flex-col gap-2 md:rounded-lg">
@@ -86,27 +136,33 @@ export default function EditAccount() {
                                     className={`pl-2 w-3/5 bg-white md:max-w-md h-10 rounded ${errors.email ? 'border-2 border-red-500' : ''}`}
                                 />
                             </div>
-                            {errors.email && <span className="text-red-600 text-sm font-medium text-right pr-2">{errors.email}</span>}
+                            {errors.email &&
+                                <span
+                                    className="text-red-600 text-sm font-medium text-right pr-2">{errors.email}</span>}
                         </div>
 
 
-                        <div className="bg-primary/20 p-6 flex flex-col gap-2 md:rounded-lg">
+                        <div className="bg-primary/20 hover:bg-primary/50 p-6 flex flex-col gap-2 md:rounded-lg">
                             <div className="flex justify-between flex-row items-center">
-                                <label className="h2 text-primary">Wachtwoord</label>
-                                <input
-                                    name="wachtwoord"
-                                    type="password"
-                                    value={formData.wachtwoord}
-                                    onChange={handleChange}
-                                    className={`pl-2 w-3/5 bg-white md:max-w-md h-10 rounded ${errors.wachtwoord ? 'border-2 border-red-500' : ''}`}
-                                />
+                                <button className="hover:cursor-pointer">Wijzig wachtwoord</button>
                             </div>
-                            {errors.wachtwoord && <span className="text-red-600 text-sm font-medium text-right pr-2">{errors.wachtwoord}</span>}
+                            {/*    <label className="h2 text-primary">Wachtwoord</label>*/}
+                            {/*    <input*/}
+                            {/*        name="wachtwoord"*/}
+                            {/*        type="password"*/}
+                            {/*        value={formData.wachtwoord}*/}
+                            {/*        onChange={handleChange}*/}
+                            {/*        className={`pl-2 w-3/5 bg-white md:max-w-md h-10 rounded ${errors.wachtwoord ? 'border-2 border-red-500' : ''}`}*/}
+                            {/*    />*/}
+                            {/*</div>*/}
+                            {/*{errors.wachtwoord && <span*/}
+                            {/*    className="text-red-600 text-sm font-medium text-right pr-2">{errors.wachtwoord}</span>}*/}
                         </div>
 
 
                         <div className="flex justify-center rounded-3xl pt-7 items-center md:justify-end md:pt-4">
                             <button
+                                onClick={handleSubmit}
                                 type="submit"
                                 className="text-white bg-primary h2 w-fit h-fit p-4 md:rounded-lg cursor-pointer hover:opacity-90 transition-opacity"
                             >
