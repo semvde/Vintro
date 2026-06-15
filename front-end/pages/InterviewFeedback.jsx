9c9ac064e29feadd2f37ead6441e2e7ea23fe9f8
Lisa <lisamao2007@gmail.com>
2026-06-15 11:57:00 +0200

    added layout

--- front-end/pages/InterviewFeedback.jsx
+++ front-end/pages/InterviewFeedback.jsx
@@ -4,7 +4,7 @@ import Victoria from "../src/assets/Victoria - Job coach.png";
 
 export default function InterviewFeedback() {
     const feedback = {
-        reaction: "Blij",
+        reaction: "happy",
         good_points: ["Je bent formeel geweest", "super leuk enthousiast"],
         improvement_points: ["Noem concretere voorbeelden van je vaardigheden", "Reageer meer inhoudelijk op vragen"],
         improved_example: "Sinds de vorige keer ben je veel enthousiaster geworden! Ga zo door!!!"
@@ -39,6 +39,19 @@ export default function InterviewFeedback() {
     // const feedback = data.data.ai_feedback;
 
     return (
+        <>
+            <div>
+            <h1>Transcriptie</h1>
+                <div className="flex flex-col">
+                    <div className="flex justify-start">
+                        <p className=" w-fit h-fit p-3">ai - text</p>
+                    </div>
+                    <div className="flex justify-end">
+                        <p className=" w-fit h-fit p-3">text - you</p>
+                    </div>
+                </div>
+            </div>
+
         <div className="p-4 max-w-2xl mx-auto pb-12 bg-secondary/20">
             <div className={"flex justify-center"}>
                 <img src={Victoria} alt={"Victoria"} width={200}/>
@@ -64,5 +77,6 @@ export default function InterviewFeedback() {
                 {feedback.improved_example}
             </p>
         </div>
+        </>
     );
 }
