b4d9f6cbd4a05fbd9470d523d789466727418c82
Lisa <lisamao2007@gmail.com>
2026-06-16 11:13:47 +0200

    changed vacancy styling

--- front-end/pages/VacancyFeedback.jsx
+++ front-end/pages/VacancyFeedback.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { fetchAPI } from "../services/Fetch.js";
+import {useEffect, useState} from "react";
+import {fetchAPI} from "../services/Fetch.js";
 import Victoria from "../src/assets/Victoria - Job coach.png";
 import {useParams} from "react-router";
 
@@ -34,37 +34,48 @@ export default function VacancyFeedback() {
     const feedback = data.data.ai_feedback;
 
     return (
-        <div className="p-4 max-w-2xl mx-auto pb-12">
+        <div className="p-4 max-w-2xl mx-auto pb-12 gap-13">
             <div className={"flex justify-center"}>
                 <img src={Victoria} alt={"Victoria"} width={200}/>
             </div>
-            <p className={"font-bold italic pb-6"}>{feedback.reaction}</p>
+            <div className="shadow-md bg-gray-300/20 mb-13  p-2">
+            <p className={"font-bold italic "}>"{feedback.reaction}"</p>
+        </div>
+            <div className="bg-secondary/20 p-3 shadow-md gap-20">
+                <div className="p-2">
+                    <h2>Wat gaat al goed?</h2>
+                    <ul className={"list-disc list-inside"}>
+                        {feedback.good_points.map((item, i) => (
+                            <li key={i}>{item}</li>
+                        ))}
+                    </ul>
 
-            <h2>Wat gaat al goed?</h2>
-            <ul className={"list-disc list-inside"}>
-                {feedback.good_points.map((item, i) => (
-                    <li key={i}>{item}</li>
-                ))}
-            </ul>
+                </div>
+                <div className="bg-white p-2 shadow-md mt-3">
+                    <h2 className="font-semibold">Verbeterpunten</h2>
+                    <ul className={"list-disc list-inside"}>
+                        {feedback.improvement_points.map((item, i) => (
+                            <li key={i}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
 
-            <h2 className="font-semibold mt-4 pt-6">Verbeterpunten</h2>
-            <ul className={"list-disc list-inside"}>
-                {feedback.improvement_points.map((item, i) => (
-                    <li key={i}>{item}</li>
-                ))}
-            </ul>
+            <div className="p-4">
+                <h2 className="font-semibold mt-4 pt-6">Suggesties voor je CV</h2>
+                <ul className={"list-disc list-inside"}>
+                    {feedback.profile_suggestions.map((item, i) => (
+                        <li key={i}>{item}</li>
+                    ))}
+                </ul>
+            </div>
 
-            <h2 className="font-semibold mt-4 pt-6">Suggesties voor je CV</h2>
-            <ul className={"list-disc list-inside"}>
-                {feedback.profile_suggestions.map((item, i) => (
-                    <li key={i}>{item}</li>
-                ))}
-            </ul>
-
-            <h2 className="font-semibold mt-4 pt-6">Voorbeeld</h2>
-            <p className="whitespace-pre-line">
-                {feedback.improved_example}
-            </p>
+            <div className="p-3 bg-primary/20 mt-4 shadow-md">
+                <h2 className="font-semibold pt-6">Voorbeeld</h2>
+                <p className="whitespace-pre-line">
+                    {feedback.improved_example}
+                </p>
+            </div>
         </div>
     );
 }
